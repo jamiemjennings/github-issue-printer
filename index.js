433f4b823e4bffec914a7c95ff8dc6b1c3ab559e
Jamie Jennings <jamiemjennings@gmail.com>
2018-02-27 22:40:47 -0500

    standard alignment

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const request = require('request')
-const fs = require('fs')
 const args = require('commander')
 const removeMd = require('remove-markdown')
-const os = require('os')
 
 const github = require('./lib/util/github')
 const ua = require('./lib/util/useragent')
@@ -23,75 +21,73 @@ args
     .parse(process.argv)
 
 if (!args.token) {
-    throw new Error('Error: Missing GitHub API token!')
+  throw new Error('Error: Missing GitHub API token!')
 }
 if (!args.owner) {
-    throw new Error('Missing GitHub repo owner!')
+  throw new Error('Missing GitHub repo owner!')
 }
 if (!args.repo) {
-    throw new Error('Missing GitHub repo name!')
+  throw new Error('Missing GitHub repo name!')
 }
 
 const URL = github.getGitHubIssuesUrl(args)
 
 log(`> GET ${URL}`)
 request({
-    method: 'GET',
-    uri: URL,
-    headers: {
-        authorization: `token ${args.token}`,
-        'user-agent': USER_AGENT
-    }
+  method: 'GET',
+  uri: URL,
+  headers: {
+    authorization: `token ${args.token}`,
+    'user-agent': USER_AGENT
+  }
 },
 (err, res, body) => {
-    log(`< ${res.statusCode}`)
-    if (err) {
-        log(err)
-        throw err
-    }
-    if (res.statusCode === 403) {
-        log('Error: Authentication failed')
-        log(body)
-        process.exit(1)
-    } else if (res.statusCode === 422) {
-        log(`Error 422: check that your filter selection is valid for this repository.`)
-        log(body)
-        process.exit(1)
-    } else if (res.statusCode !== 200) {
-        log(`Error: unexpected status: ${res.statusCode}`)
-        log(body)
-        process.exit(1)
-    }
-    processIssuesJson(body)
+  log(`< ${res.statusCode}`)
+  if (err) {
+    log(err)
+    throw err
+  }
+  if (res.statusCode === 403) {
+    log('Error: Authentication failed')
+    log(body)
+    process.exit(1)
+  } else if (res.statusCode === 422) {
+    log(`Error 422: check that your filter selection is valid for this repository.`)
+    log(body)
+    process.exit(1)
+  } else if (res.statusCode !== 200) {
+    log(`Error: unexpected status: ${res.statusCode}`)
+    log(body)
+    process.exit(1)
+  }
+  processIssuesJson(body)
 })
 
-function processIssuesJson(json) {
-    const issues = JSON.parse(json)
-    const newIssues = []
+function processIssuesJson (json) {
+  const issues = JSON.parse(json)
+  const newIssues = []
 
-    if (!issues || issues.length === 0) {
-        throw new Error('Error: no issues found.')
+  if (!issues || issues.length === 0) {
+    throw new Error('Error: no issues found.')
+  }
+  log(`${issues.length} issues retrieved.`)
+  issues.forEach((issue) => {
+    let repoName = args.repo
+    let issueBody = issue.body
+    issueBody = issueBody.replace(/\r/g, '') // pdfkit doesn't handle \r properly (\n is ok)
+    issueBody = removeMd(issueBody, { stripListLeaders: false })
+    let issueData = {
+      number: issue.number,
+      title: issue.title,
+      repo: repoName,
+      body: issueBody
     }
-    log(`${issues.length} issues retrieved.`)
-    issues.forEach((issue) => {
-        let issueNum = `#${issue.number}`
-        let cardTitle = issue.title
-        let repoName = args.repo
-        let issueBody = issue.body
-        issueBody = issueBody.replace(/\r/g, '') // pdfkit doesn't handle \r properly (\n is ok)
-        issueBody = removeMd(issueBody, { stripListLeaders: false })
-        let issueData = {
-            number: issue.number,
-            title: issue.title,
-            repo: repoName,
-            body: issueBody
-        }
-        newIssues.push(issueData)
-    })
+    newIssues.push(issueData)
+  })
 
-    pdf.createPdf(newIssues, {renderBody: args.body})
+  pdf.createPdf(newIssues, {renderBody: args.body})
 }
 
-function log(msg) {
-    console.error(msg)
+function log (msg) {
+  console.error(msg)
 }
