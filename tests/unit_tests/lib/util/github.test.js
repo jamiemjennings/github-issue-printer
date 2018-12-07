8a37c042e29c177311399ed29e323824af70f191
Jamie Jennings <jamiemjennings@users.noreply.github.com>
2018-12-06 22:45:29 -0500

    More unit tests (#24)

--- tests/unit_tests/lib/util/github.test.js
+++ tests/unit_tests/lib/util/github.test.js
@@ -21,6 +21,22 @@ describe('GitHub util', function () {
         }
       })
     })
+    it('succeeds if owner & repo is specified', () => {
+      let url = github.getGitHubIssuesQueryUrl({owner: 'foobar', repo: 'fizzbuzz'})
+      assert.equal(url, 'https://api.github.com/repos/foobar/fizzbuzz/issues?')
+    })
+    it('succeeds if owner & repo is specified with labels', () => {
+      let url = github.getGitHubIssuesQueryUrl({owner: 'foobar', repo: 'fizzbuzz', labels: 'abc'})
+      assert.equal(url, 'https://api.github.com/repos/foobar/fizzbuzz/issues?labels=abc')
+    })
+    it('succeeds if owner & repo is specified with milestone', () => {
+      let url = github.getGitHubIssuesQueryUrl({owner: 'foobar', repo: 'fizzbuzz', milestone: 'foo'})
+      assert.equal(url, 'https://api.github.com/repos/foobar/fizzbuzz/issues?milestone=foo')
+    })
+    it('succeeds if owner & repo is specified with labels & milestone', () => {
+      let url = github.getGitHubIssuesQueryUrl({owner: 'foobar', repo: 'fizzbuzz', labels: 'abc,def', milestone: 'foo'})
+      assert.equal(url, 'https://api.github.com/repos/foobar/fizzbuzz/issues?milestone=foo&labels=abc%2Cdef')
+    })
   })
   describe('getGitHubIssueUrl()', function () {
     invalidOptionsTests.forEach((testData) => {
@@ -32,6 +48,26 @@ describe('GitHub util', function () {
           done()
         }
       })
+    })
+    it('succeeds if owner, repo & issueNum is specified', function () {
+      let url = github.getGitHubIssueUrl({owner: 'foobar', repo: 'fizzbuzz', issueNum: '3'})
+      assert.equal(url, 'https://api.github.com/repos/foobar/fizzbuzz/issues/3')
+    })
+  })
+  describe('getGitHubProjectsUrl()', function () {
+    invalidOptionsTests.forEach((testData) => {
+      it(`throws error if options is ${JSON.stringify(testData.options)}`, (done) => {
+        try {
+          github.getGitHubProjectsUrl(testData.options)
+        } catch (err) {
+          assert.isNotNull(err)
+          done()
+        }
+      })
+    })
+    it('succeeds if owner is specified', function () {
+      let url = github.getGitHubProjectsUrl({owner: 'foobar'})
+      assert.equal(url, 'https://api.github.com/orgs/foobar/projects?status=open')
     })
   })
 })
